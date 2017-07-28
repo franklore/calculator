3780e610f06fbacfc5d0d2af4d2aa5d46d14d833
weixu000 <wei-x15@mails.tsinghua.edu.cn>
2017-07-28 11:30:47 +0800

    history

--- scripts/history.js
+++ scripts/history.js
@@ -7,7 +7,30 @@ function saveHistory(str, data) {
     p.dataset.ans = data;
     p.onclick = retriveHistory;
     historyDiv.appendChild(p);
+
+    var his = localStorage.getItem('history');
+    if (!his) {
+        his = {};
+    } else {
+        his = JSON.parse(his);
+    }
     if (historyDiv.childElementCount > maxHistory) {
+        delete his[historyDiv.firstElementChild.textContent];
         historyDiv.removeChild(historyDiv.firstElementChild);
+    }
+    his[str] = data;
+    localStorage.setItem('history', JSON.stringify(his));
+}
+
+function initHistory() {
+    var his = localStorage.getItem('history');
+    if (his) {
+        his = JSON.parse(his);
+        localStorage.removeItem('history');
+    } else {
+        return;
+    }
+    for (str in his) {
+        saveHistory(str, his[str]);
     }
 }
